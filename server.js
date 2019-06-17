10790711de8c9536eb953cf02e970b140b9f497d
Mugerwa Joseph <kodek-sleuth@Mugerwas-MacBook-Pro.local>
2019-06-17 15:44:27 +0200

    Project Structure

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
+  res.status(error.status || 400);
   res.json({
     Error: error.message,
   });
